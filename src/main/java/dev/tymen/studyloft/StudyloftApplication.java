2cc51c0297e30a6bc57b45798d47d558912c332d
Tymen Vis <Tymenvis@outlook.com>
2025-02-11 10:45:31 +0100

    Initial commit

---
+++ src/main/java/dev/tymen/studyloft/StudyloftApplication.java
@@ -0,0 +1,13 @@
+package dev.tymen.studyloft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyloftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyloftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tymen/studyloft/StudyloftApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tymen.studyloft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyloftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
